fbf1a927e055f2ef1991c87e942644aa3567f3ad
sitypop <sityemilyshah@gmail.com>
2016-04-18 17:48:32 +0100

    Added touch_in method which sets instance variable #in_journey state to true

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,19 +3,28 @@ class OysterCard
   DEFAULT_BALANCE = 0
   MAXIMUM = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
+    fail "New balance exceeds #{MAXIMUM}!" if limit_exceeded?(amount)
     @balance += amount
-    fail "New balance exceeds #{MAXIMUM}!" if limit_exceeded?
   end
 
-  def limit_exceeded?
+  def deduct_fare(amount)
+    @balance -= amount
+  end
+
+  def limit_exceeded?(amount)
     @balance + amount > MAXIMUM
+  end
+
+  def touch_in
+    @in_journey = true
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,7 @@ describe OysterCard do
     expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
   end
 
-  describe '#top_up method' do
+  describe '#top_up' do
 
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
@@ -17,6 +17,28 @@ describe OysterCard do
     it 'raises an error if new balance would exceed MAXIMUM' do
       message = "New balance exceeds #{OysterCard::MAXIMUM}!"
       expect{ subject.top_up 100 }.to raise_error message
+    end
+
+  end
+
+  describe '#deduct_fare' do
+
+    it { is_expected.to respond_to(:deduct_fare).with(1).argument }
+
+    it 'changes balance with deduct_fare amount' do
+      subject.top_up(40)
+      expect { subject.deduct_fare 20 }.to change { subject.balance }.by -20
+    end
+
+  end
+
+  describe '#touch_in' do
+
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'sets #in_journey to true' do
+      subject.touch_in
+      expect(subject.in_journey).to eq true
     end
 
   end
