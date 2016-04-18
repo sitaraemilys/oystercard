030c9a157351eb855d936ca28dfc31f98bcd8401
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-18 17:39:22 +0100

    topping up oyster balance by specified amount

--- oystercard_feature_spec.rb
+++ oystercard_feature_spec.rb
@@ -2,9 +2,19 @@ require "./lib/oystercard.rb"
 
 describe Oystercard do
   subject(:card) { Oystercard.new }
-  describe "checking a balance" do
+
+  context "checking an initial balance" do
     it "is created with a balance of zero" do
       expect(card.balance).to eq 0
+    end
+  end
+
+  describe "topping up" do
+    it "adds the specified top up value" do
+      card.top_up(10)
+      expect(card.balance).to eq 10
+      card.top_up(12)
+      expect(card.balance).to eq 22
     end
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,16 @@
 require "oystercard"
+
 describe Oystercard do
   subject(:oystercard) { described_class.new }
 
   it "has a starting balance of zero" do
     expect(oystercard.balance).to eq 0
+  end
+  describe "#top_up" do
+    let(:amount) { rand(100) }
+    it "tops up the balance with the specified amount" do
+      oystercard.top_up(amount)
+      expect(oystercard.balance).to eq amount
+    end
   end
 end
